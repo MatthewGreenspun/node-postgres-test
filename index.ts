a9fd917364eaa8892ae2100dab9037026d093c0a
Matthew Greenspun <matthewgreenspun@gmail.com>
2021-04-28 20:38:21 -0400

    created route to show posts table, and route to show every user's posts

--- index.ts
+++ index.ts
@@ -11,18 +11,18 @@ const pool = new pg.Pool({
 });
 const app = express();
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT * FROM person");
-  res.send(`
+const css = ` 
     <style>
-      table {
+      * {
         font-family: arial, sans-serif;
+      }
+      table {
         border-collapse: collapse;
         width: 100%;
       }
       
       td, th {
-        border: 1px solid #dddddd;
+        border: 1px solid black;
         text-align: left;
         padding: 8px;
       }
@@ -31,6 +31,38 @@ app.get("/", async (req, res) => {
         background-color: #dddddd;
       }
     </style>
+`;
+
+app.get("/", async (req, res) => {
+  const { rows } = await pool.query(
+    "SELECT person.name AS name, post.post_text AS post_text FROM post RIGHT JOIN person ON person.id = post.person_id"
+  );
+  res.send(`
+    ${css}
+    <h1>People and their posts: </h1>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Post text</th>
+      </tr>
+      ${rows
+        .map(
+          (row: { name: string; post_text: string | null }) => `
+        <tr>
+          <td>${row.name}</td>
+          <td>${row.post_text ?? "No posts from this user"}</td>
+        </tr>`
+        )
+        .toString()
+        .replace(/,/g, "")}
+    </table>
+  `);
+});
+
+app.get("/people", async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM person");
+  res.send(`
+    ${css}
     <h1>Person table: </h1>
     <table>
       <tr>
@@ -40,11 +72,38 @@ app.get("/", async (req, res) => {
       </tr>
       ${rows
         .map(
-          (row) => `
+          (row: { id: string; name: string; date_of_birth: Date }) => `
         <tr>
           <td>${row.id}</td>
           <td>${row.name}</td>
-          <td>${(<Date>row.date_of_birth).toString().slice(0, 15)}</td>
+          <td>${row.date_of_birth.toString().slice(0, 15)}</td>
+        </tr>
+      `
+        )
+        .toString()
+        .replace(/,/g, "")}
+    </table>
+  `);
+});
+
+app.get("/posts", async (req, res) => {
+  const { rows } = await pool.query("SELECT * FROM post");
+  res.send(`
+    ${css}
+    <h1>Posts table: </h1>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Person ID</th>
+        <th>Post text</th>
+      </tr>
+      ${rows
+        .map(
+          (row: { id: string; person_id: string; post_text: string }) => `
+        <tr>
+          <td>${row.id}</td>
+          <td>${row.person_id}</td>
+          <td>${row.post_text}</td>
         </tr>
       `
         )
